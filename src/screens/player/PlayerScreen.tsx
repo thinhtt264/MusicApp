6c9bd194744e7ccad3d605732e49fc2532f24720
dongchiteo <vilk@payme.vn>
2023-09-12 13:55:15 +0700

    test

--- src/screens/player/PlayerScreen.tsx
+++ src/screens/player/PlayerScreen.tsx
@@ -122,7 +122,7 @@ const PlayerScreen = ({ route }: any) => {
     ) : (
       <Blurhash blurhash={bgColor} style={styles.blurHashBackground} />
     );
-
+  //test
 
   return !bgColor ? (
     <></>
